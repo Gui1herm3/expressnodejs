a35a99a15b4b4602f535a5f590aa2b2853d8db71
Jao <125588400+Gui1herm3@users.noreply.github.com>
2023-09-28 11:01:25 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 8018; // Porta em que o servidor será executado
-// Rota para exibir a página index.html
+const port = 8018; 
+
 app.get('/', (req, res) => {
 res.sendFile(__dirname + '/index.html');
 });
 
-// Rota para exibir a página outra_pagina.html
 app.get('/login2.html', (req, res) => {
 res.sendFile(__dirname + '/login2.html');
 });
@@ -19,7 +18,6 @@ app.get('/script.js', (req, res) => {
 res.sendFile(__dirname + '/script.js');
 });
 
-// Iniciar o servidor
 app.listen(port, () => {
 console.log(`Servidor Express está rodando na porta ${port}`);
 });
